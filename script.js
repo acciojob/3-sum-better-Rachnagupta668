00acd9e800cdf3bf3ad764dbe5eec7fd1c993eed
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-05-20 21:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-function threeSumClosest(nums, target) {
-  nums.sort((a, b) => a - b); // Sort the array in ascending order
+function threeSum(arr, target) {
+  arr.sort((a, b) => a - b); // sort the array in ascending order
   let closestSum = Infinity;
-
-  for (let i = 0; i < nums.length - 2; i++) {
+  
+  for (let i = 0; i < arr.length - 2; i++) {
     let left = i + 1;
-    let right = nums.length - 1;
-
+    let right = arr.length - 1;
+    
     while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
-
-      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+      let sum = arr[i] + arr[left] + arr[right];
+      let diff = Math.abs(target - sum);
+      
+      if (diff === 0) {
+        // we found a sum that is equal to the target, so we can return it
+        return sum;
+      }
+      
+      if (diff < Math.abs(target - closestSum)) {
+        // update the closestSum if the current sum is closer to the target
         closestSum = sum;
       }
-
+      
       if (sum < target) {
+        // if the sum is less than target, we need to increase the sum
         left++;
       } else {
+        // if the sum is greater than target, we need to decrease the sum
         right--;
       }
     }
   }
-
+  
   return closestSum;
 }
 
-// Example usage:
- // const nums = [-1, 2, 1, -4];
- // const target = 1;
- // const closestSum = threeSumClosest(nums, target);
- // console.log(closestSum); // Output: 2
+module.exports = threeSum;
