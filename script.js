109372dc5a9f9cb5526d43d3bcd6717f705799de
Rachna <106364652+Rachnagupta668@users.noreply.github.com>
2023-05-20 16:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	nums.sort((a,b)(a-b)){
+		colsesum=infiniye;
+		for(int i =0; i<arr.length;i++){
+			left=i+1;
+			
+			right=arr.length-1;
+							while(left<right){
+
+			const sum=arr[i]+arr[left]+arr[right];
+			if(Math.abs(sum-target)<Math.abs(colssum-target)){
+				sum=colsesum;
+			}
+								if(sum<target){
+									left++;
+									
+								}else{
+									right--;
+								}
+			}
+		}
+	}
+  return colsesum;
 }
 
 module.exports = threeSum;
